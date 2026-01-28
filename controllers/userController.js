46547494fbba5c7b237c50fad732207ca7a71c52
manishngp <manishngpdoc@gmail.com>
2026-01-28 10:35:08 +0530

    backend hoisted

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,22 @@
-const getUsers = (req, res) => {
-  res.json({
-    message: "Get all users API"
-  });
+const User = require("../models/User");
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
-const createUser = (req, res) => {
-  res.json({
-    message: "Create user API",
-    data: req.body
-  });
+const createUser = async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 module.exports = { getUsers, createUser };
